9853990371bab4c106f55a2a76fdfe36448166c6
Francisco Barber Otero <franciscobarber@gmail.com>
2025-11-06 17:26:57 +0000

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Header';
-import { CartProvider } from './components/CartContext';
 import ProductCatalog from './components/Catalog.jsx';
 import Cart from './components/Cart';
 import Checkout from './components/Checkout';
