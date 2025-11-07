89e3d98f3915dcd4b50eacf154c925c5915b1c10
Francisco Barber Otero <franciscobarber@gmail.com>
2025-11-07 14:24:40 +0000

    add

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { msalInstance } from './components/AuthContext';
+import { EventType } from '@azure/msal-browser';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+// This will ensure MSAL is ready to handle redirects before the app renders
+msalInstance.initialize().then(() => {
+  // Account selection logic is handled in AuthContext.js
+  // The event callback lets us logging and debugging.
+  msalInstance.addEventCallback((event) => {
+    if (event.eventType === EventType.LOGIN_SUCCESS && event.payload.account) {
+      msalInstance.setActiveAccount(event.payload.account);
+    }
+  });
+
+  root.render(<App />);
+});
